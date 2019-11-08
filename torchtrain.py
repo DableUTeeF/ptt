67d4d00de5bbaf7a330f309a9b45ec13b82d0c37
Natthapawn Hongcharoen <palm22180@gmail.com>
2019-11-08 18:40:05 +0700

    Change train to the most recent in ETIC

--- torchtrain.py
+++ torchtrain.py
@@ -1,178 +1,220 @@
 import os
-import warnings
-warnings.simplefilter("ignore")
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,2"
+from efficientnet_pytorch import EfficientNet
 import torch
-import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch import nn
 import torch.nn.parallel
-import torch.nn.functional
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
 import torch.optim
+import torch.multiprocessing as mp
 import torch.utils.data
 import torch.utils.data.distributed
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-from torch.optim.lr_scheduler import MultiStepLR
-from easydict import EasyDict
-from utils import Progbar
-from efficientnet_pytorch import EfficientNet
+import time
 from sklearn.metrics import f1_score
 import numpy as np
+from torch.optim.lr_scheduler import ExponentialLR
+from utils import Progbar
+from histogram_crop import hist_crop
+torch.manual_seed(0)
+np.random.seed(0)
 
 
-def getmodel(cls):
-    model = EfficientNet.from_pretrained('efficientnet-b6')
-    num_ftrs = model._fc.in_features
-    model._fc = nn.Linear(num_ftrs, cls)
-    return model
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(1.0 / batch_size))
+    return res
 
 
 if __name__ == '__main__':
-    args = EasyDict({
-        'batch_size': 2,
-        'batch_mul': 8,
-        'val_batch_size': 2,
-        'cuda': True,
-        'model': '',
-        'train_plot': False,
-        'epochs': 180,
-        'try_no': 'baselinev3_B6',
-        'imsize': 528,
-        'imsize_l': 550,
-        'traindir': '/media/palm/62C0955EC09538ED/ptt/train/',
-        'valdir': '/media/palm/62C0955EC09538ED/ptt/val/',
-        'workers': 8,
-        'resume': False,
-        'multi_gpu': False
-    })
-    best_acc = 0
-    best_no = 0
-    start_epoch = 1
-    model = getmodel(17).cuda()
+    root = '/home/root1/Desktop/ott/'
+    batch_size = 8
+    h1 = hist_crop(528, )
+    h2 = hist_crop(528, )
+    val_transform = transforms.Compose([h1,
+                                        transforms.Resize(528),
+                                        transforms.CenterCrop(528),
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.RandomVerticalFlip(),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
+    train_transform = transforms.Compose([h2,
+                                          transforms.Resize(528),
+                                          transforms.CenterCrop(528),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-    optimizer = torch.optim.SGD(model.parameters(), 0.1,
-                                momentum=0.9,
-                                weight_decay=1e-4,
-                                )
-    scheduler = MultiStepLR(optimizer, [2, 5])
+    trainset = torchvision.datasets.ImageFolder(root+'train', transform=train_transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=8)
+
+    testset = torchvision.datasets.ImageFolder(root+'val', transform=val_transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=8)
+    model = EfficientNet.from_pretrained('efficientnet-b6')
+    model._fc = nn.Linear(model._fc.in_features, 17)
+    model = nn.DataParallel(model)
+    model.cuda()
     criterion = nn.CrossEntropyLoss().cuda()
-    zz = 0
-    train_dataset = datasets.ImageFolder(
-        args.traindir,
-        transforms.Compose([
-            transforms.Resize(args.imsize),
-            transforms.CenterCrop(args.imsize),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]))
-    trainloader = torch.utils.data.DataLoader(train_dataset,
-                                              batch_size=args.batch_size,
-                                              shuffle=True,
-                                              num_workers=args.workers,
-                                              pin_memory=False)
-    val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(args.valdir, transforms.Compose([
-            transforms.Resize(args.imsize),
-            transforms.CenterCrop(args.imsize),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=args.val_batch_size,
-        num_workers=args.workers,
-        pin_memory=False)
-    if args.multi_gpu:
-        model = torch.nn.DataParallel(model).cuda()
-    # cudnn.benchmark = args.batch_size > 1
-    if args.resume:
-        if args.resume is True:
-            args['resume'] = f'./checkpoint/{args.try_no}best.t7'
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
-            # start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['acc']
-            model.load_state_dict(checkpoint['net'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
 
+    optimizer = torch.optim.SGD(model.parameters(), 1e-2,
+                                momentum=0.9,
+                                weight_decay=1e-5)
 
-    def train(epoch):
-        print(f'\033[{np.random.randint(31, 37)}m', end='')
-        print('\nEpoch: %d/%d' % (epoch, args.epochs))
+    lr_schudule = ExponentialLR(optimizer, 0.97)
+
+    def train(train_loader, model, criterion, optimizer, epoch, ):
+        batch_time = AverageMeter('Time', ':6.3f')
+        data_time = AverageMeter('Data', ':6.3f')
+        losses = AverageMeter('Loss', ':.4e')
+        top1 = AverageMeter('Acc@1', ':6.2f')
+        progress = Progbar(len(train_loader))
+
+        # switch to train mode
         model.train()
-        optimizer.zero_grad()
-        criterion.zero_grad()
-        progbar = Progbar(len(trainloader))
-        for batch_idx, (inputs, targets) in enumerate(trainloader):
-            inputs, targets = inputs.to('cuda'), targets.to('cuda')
-            outputs = model(inputs)
-            loss = criterion(outputs, targets) / args.batch_mul
-            try:
-                loss.backward()
-            except RuntimeError:
-                print(' - error', inputs.size(), targets.size())
-                continue
-            _, predicted = outputs.max(1)
-            f1_total = f1_score(targets.float().cpu().numpy(), predicted.float().cpu().numpy(), average='macro')
-            lfs = (batch_idx + 1) % args.batch_mul
-            if lfs == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-            suffix = [('loss', loss.item() * args.batch_mul),
-                      ('acc', predicted.eq(targets).sum().item() / targets.size(0)),
-                      ('f1', f1_total)]
-            progbar.update(batch_idx + 1, suffix)
 
-        optimizer.step()
-        optimizer.zero_grad()
+        end = time.time()
+        for i, (images, target) in enumerate(train_loader):
+            # if i >= 200:
+            #     print()
+            #     break
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            images = images.cuda()
+            target = target.cuda()
+
+            # compute output
+            output = model(images)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1 = accuracy(output, target, topk=(1,))
+            # losses.update(loss.item(), images.size(0))
+            # top1.update(acc1[0], images.size(0))
+
+            # compute gradient and do SGD step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+            suffix = [('loss', loss.item()), ('acc', acc1[0].cpu().numpy())]
+            progress.update(i+1, suffix)
 
 
-    def test(epoch):
-        global best_acc, best_no
+    def validate(val_loader, model, criterion, ):
+        batch_time = AverageMeter('Time', ':6.3f')
+        losses = AverageMeter('Loss', ':.4e')
+        top1 = AverageMeter('Acc@1', ':6.2f')
+        progress = Progbar(len(val_loader))
+
+        # switch to evaluate mode
         model.eval()
-        test_loss = 0
-        progbar = Progbar(len(val_loader))
-        predicteds = np.zeros((len(val_loader.sampler)), dtype='float32')
-        targets_ = np.zeros(len(val_loader.sampler), dtype='float32')
+        predicted = np.array([], dtype='float32')
+        targets = np.array([], dtype='float32')
         with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(val_loader):
-                inputs, targets = inputs.to('cuda'), targets.to('cuda')
-                outputs = model(inputs)
-                loss = criterion(outputs, targets)
-                test_loss += loss.item()
-                _, predicted = outputs.max(1)
-                f1 = f1_score(targets.float().cpu().numpy(), predicted.float().cpu().numpy(), average='macro')
-                predicteds[batch_idx*args.val_batch_size:(batch_idx+1)*args.val_batch_size] = predicted.float().cpu().numpy()
-                targets_[batch_idx*args.val_batch_size:(batch_idx+1)*args.val_batch_size] = targets.float().cpu().numpy()
-                suffix = [('loss', loss.item()),
-                          ('acc', predicted.eq(targets).sum().item() / targets.size(0)),
-                          ('f1', f1)]
-                progbar.update(batch_idx + 1, suffix)
+            end = time.time()
+            for i, (images, target) in enumerate(val_loader):
+                images = images.cuda()
+                target = target.cuda()
 
-        f1_total = f1_score(targets_, predicteds, average='macro')
-        # Save checkpoint.
-        acc = 100. * f1_total / len(val_loader)
-        state = {
-            'optimizer': optimizer.state_dict(),
-            'net': model.state_dict(),
-            'acc': acc,
-            'epoch': epoch,
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        if acc > best_acc:
-            torch.save(state, f'./checkpoint/{args.try_no}best.t7')
-            best_acc = acc
-        torch.save(model.state_dict(), f'./checkpoint/{args.try_no}temp.t7')
+                # compute output
+                output = model(images)
+                loss = criterion(output, target)
 
+                # measure accuracy and record loss
+                predicted = np.append(predicted, np.argmax(output, axis=1))
+                targets = np.append(targets, target)
+                acc1 = accuracy(output, target, topk=(1,))
+                losses.update(loss.item(), images.size(0))
+                top1.update(acc1[0], images.size(0))
 
-    for epoch in range(start_epoch, start_epoch + args.epochs):
-        scheduler.step()
-        train(epoch)
-        test(epoch)
-        print(f'best: {best_acc}')
+                # measure elapsed time
+                batch_time.update(time.time() - end)
+                end = time.time()
+
+                suffix = [('loss', loss.item()), ('acc', acc1[0].cpu().numpy())]
+                progress.update(i + 1, suffix)
+        f1 = f1_score(targets, predicted, average='macro')
+        print(f1)
+        return top1.avg, f1
+    save_folder = len(os.listdir('checkpoint'))
+    os.mkdir(os.path.join('checkpoint', str(save_folder)))
+    for i in range(40):
+        print(f'\033[{np.random.randint(31, 37)}m', 'Epoch:', i+1)
+        train(trainloader, model, criterion, optimizer, i+1)
+        torch.save(model.state_dict(), f'checkpoint/temp.torch')
+        acc, f1 = validate(testloader, model, criterion)
+        h1.total_error = 0
+        h2.total_error = 0
+        dct = {'net': model.state_dict(),
+               'opt': optimizer.state_dict(),
+               'acc': acc,
+               'f1': f1
+               }
+        torch.save(dct, f'checkpoint/{save_folder}/{i}_{f1:.4f}.torch')
+        lr_schudule.step()
